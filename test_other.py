c806931083ee1e5c66374eeb6d673d92c7035d91
magda <magdamoraga14@gmail.com>
2025-09-28 23:49:11 +0200

    added test file for additional code

---
+++ test_other.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy import stats
+
+# Test t dla dwóch grup
+group1 = np.random.normal(5, 1, 100)
+group2 = np.random.normal(5.5, 1, 100)
+
+t_stat, p_val = stats.ttest_ind(group1, group2)
+print("p-value:", p_val)  # czy różnica jest istotna?
